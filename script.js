11d148fd38294ffd88bb745c655108dcfc17b626
Denny <examle@gmail.com>
2020-12-13 19:27:29 +0200

    first

---
+++ script.js
@@ -0,0 +1,15 @@
+ "use strict";
+
+//  const result = confirm("Are you here?");
+//  console.log(result);
+
+    // const answer = prompt("Are you 18 years old?","18");
+    // console.log (typeof(answer));
+
+    const answers = [];
+    
+    answers [0] = prompt ("Как Ваше имя?","");
+    answers [1] = prompt ("Как Ваше фамилия?","");
+    answers [2] = prompt ("Сколько лет?","");
+
+    document.write (answers);
